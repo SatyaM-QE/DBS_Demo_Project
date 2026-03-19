a6d6cbdec7ac404a422188364f5aa7d115da7f94
SatyaM-QE <satya.dipu91@gmail.com>
2026-03-19 11:44:32 +0530

    updated common java class

--- src/test/java/pages/CommonMethod.java
+++ src/test/java/pages/CommonMethod.java
@@ -7,17 +7,16 @@ import java.awt.event.KeyEvent;
 import org.openqa.selenium.WebDriver;
 
 public class CommonMethod {
-public static void clickAutoSuggest(WebDriver driver) throws AWTException, InterruptedException{
-	//driver.findElement(By.xpath(("//ul[@id='react-autosuggest-1']/li[1])"))).click();
-	Robot rb = new Robot();
+	public static void clickAutoSuggest(WebDriver driver) throws AWTException, InterruptedException {
+		// driver.findElement(By.xpath(("//ul[@id='react-autosuggest-1']/li[1])"))).click();
+		Robot rb = new Robot();
 
-	rb.keyPress(KeyEvent.VK_DOWN);
-	//rb.keyRelease(KeyEvent.VK_DOWN);
-	rb.keyPress(KeyEvent.VK_ENTER);
-	rb.keyRelease(KeyEvent.VK_ENTER);
-	rb.keyPress(KeyEvent.VK_UP);
-	rb.keyRelease(KeyEvent.VK_UP);
-	Thread.sleep(3000);
-}
-
+		rb.keyPress(KeyEvent.VK_DOWN);
+		// rb.keyRelease(KeyEvent.VK_DOWN);
+		rb.keyPress(KeyEvent.VK_ENTER);
+		rb.keyRelease(KeyEvent.VK_ENTER);
+		rb.keyPress(KeyEvent.VK_UP);
+		rb.keyRelease(KeyEvent.VK_UP);
+		Thread.sleep(3000);
+	}
 }
